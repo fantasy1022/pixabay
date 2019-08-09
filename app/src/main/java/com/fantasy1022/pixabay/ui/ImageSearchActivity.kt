2dbb393f0e1f56dabb11358ad6522957f87d8875
fantasy1022 <fantasy-1022@hotmail.com>
2019-08-09 18:14:20 +0800

    Modify search page UI.

--- app/src/main/java/com/fantasy1022/pixabay/ui/ImageSearchActivity.kt
+++ app/src/main/java/com/fantasy1022/pixabay/ui/ImageSearchActivity.kt
@@ -1,16 +1,10 @@
 package com.fantasy1022.pixabay.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import androidx.lifecycle.ViewModelStore
 import com.fantasy1022.pixabay.R
 import com.fantasy1022.pixabay.utilities.InjectorUtils
-import kotlinx.android.synthetic.main.activity_main.*
 
 class ImageSearchActivity : AppCompatActivity() {
 
@@ -20,16 +14,7 @@ class ImageSearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val factory = InjectorUtils.provideImageSearchViewModel()
-        //ViewModelProviders.of(this, factory).get(ImageSerachViewModel::class.java)
         viewModel = ViewModelProvider(this, factory).get(ImageSerachViewModel::class.java)
-        viewModel = ViewModelProvider(this, factory).get(ImageSerachViewModel::class.java)
-
-
-        testBtn.setOnClickListener {
-            viewModel.getSearchImages("app").observe(this, Observer { imagesInfo ->
-                Log.d("Fan", "${imagesInfo.totalHits}")
-            })
-        }
     }
 
 
