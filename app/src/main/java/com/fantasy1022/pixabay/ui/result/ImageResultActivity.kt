9c6ada65a94c2e110620b6aea59abd1f36bf986a
FantasyFang <fantasyfang@kkbox.com>
2019-08-12 21:39:22 +0800

    Add style menu in search result page.

--- app/src/main/java/com/fantasy1022/pixabay/ui/result/ImageResultActivity.kt
+++ app/src/main/java/com/fantasy1022/pixabay/ui/result/ImageResultActivity.kt
@@ -3,9 +3,18 @@ package com.fantasy1022.pixabay.ui.result
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager.VERTICAL
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.lifecycle.lifecycleOwner
+import com.afollestad.materialdialogs.list.listItemsSingleChoice
 import com.fantasy1022.pixabay.R
 import com.fantasy1022.pixabay.utilities.InjectorUtils
 import kotlinx.android.synthetic.main.activity_image_result.*
@@ -23,9 +32,29 @@ class ImageResultActivity : AppCompatActivity(), ImageAdapter.Callback {
         }
     }
 
+    enum class LayoutManagerType(val value: Int) {
+        STAGGER(0), GRID(1), LINEAR(2);
+
+        companion object {
+            fun toEnum(value: Int): LayoutManagerType = values().first { it.value == value }
+        }
+    }
+
     private lateinit var viewModel: ImageResultViewModel
     private lateinit var query: String
     private lateinit var imageAdapter: ImageAdapter
+    private lateinit var currentLayoutMangerType: LayoutManagerType
+    private val staggeredGridLayoutManager by lazy {
+        StaggeredGridLayoutManager(2, VERTICAL)
+    }
+    private val gridLayoutManager by lazy {
+        GridLayoutManager(this, 2)
+    }
+    private val linearLayoutManager by lazy {
+        LinearLayoutManager(this)
+    }
+
+    private val styleList = listOf("Stagger", "Grid", "Linear")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,9 +73,53 @@ class ImageResultActivity : AppCompatActivity(), ImageAdapter.Callback {
         })
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_layout, menu)
+        return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu): Boolean {
+        val styleView = menu.findItem(R.id.layoutStyle).actionView
+        styleView.setOnClickListener {
+            MaterialDialog(this).show {
+                title(R.string.choose_style)
+                //TODO: get initial selection
+                listItemsSingleChoice(items = styleList, initialSelection = 1) { _, index, text ->
+                    switchLayoutManager(LayoutManagerType.toEnum(index))
+                }
+                lifecycleOwner(this@ImageResultActivity)
+            }
+
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
     private fun setUpRecyclerView() {
         imageAdapter = ImageAdapter(callback = this)
-        imageRecyclerView.adapter = imageAdapter
+        with(imageRecyclerView) {
+            adapter = imageAdapter
+            layoutManager = staggeredGridLayoutManager
+        }
+    }
+
+    private fun switchLayoutManager(currentLayoutMangerType: LayoutManagerType) {
+        // If a layout manager has already been set, get current scroll position.
+        var scrollPosition: IntArray = imageRecyclerView.layoutManager?.let {
+            (it as StaggeredGridLayoutManager).findFirstCompletelyVisibleItemPositions(null)
+            //TODO:Check other manager
+        } ?: IntArray(2)
+
+        val currentManager: RecyclerView.LayoutManager = when (currentLayoutMangerType) {
+            LayoutManagerType.STAGGER -> staggeredGridLayoutManager
+            LayoutManagerType.GRID -> gridLayoutManager
+            LayoutManagerType.LINEAR -> linearLayoutManager
+        }
+
+        with(imageRecyclerView) {
+            layoutManager = currentManager
+            scrollToPosition(scrollPosition[0])
+        }
+
     }
 
     override fun onClick(transitionData: ImageAdapter.ItemViewHolder.TransitionData) {
