840d8fa3762f85844e3580f98a36a58e90b67986
FantasyFang <fantasyfang@kkbox.com>
2019-08-12 21:36:46 +0800

    Implement function of keyboard enter to search the image.

--- app/src/main/java/com/fantasy1022/pixabay/ui/main/ImageSearchActivity.kt
+++ app/src/main/java/com/fantasy1022/pixabay/ui/main/ImageSearchActivity.kt
@@ -1,6 +1,7 @@
 package com.fantasy1022.pixabay.ui.main
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
 import com.fantasy1022.pixabay.R
 import com.fantasy1022.pixabay.ui.result.ImageResultActivity
@@ -11,9 +12,17 @@ class ImageSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //TODO: handle keyboard enter
         searchImage.setOnClickListener {
             startActivity(ImageResultActivity.createIntent(this, searchEditText.text.toString().trim()))
+        }
+
+        searchEditText.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_ENTER) {
+                searchImage.callOnClick()
+                true
+            } else {
+                false
+            }
         }
     }
 
